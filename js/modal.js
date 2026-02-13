2a1d675ea662ff0e8b7107793cfc21684a2ab26c
4Davve <vadim.veng@gmail.com>
2026-02-13 11:54:19 +0100

    Update fix bag 13.02.2026

--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,14 @@
 const modal = document.querySelector('.backdrop');
 const modalBtnOpen = document.querySelector('.modal-btn-open');
+const modalBtnMobileOpen = document.querySelector('.modal-btn-mobile-open');
 const modalBtnClose = document.querySelector('.modal-btn-close');
 
 const openModal = () => modal.classList.remove('is-hidden');
+const openMobileOpen = () => modal.classList.remove('is-hidden');
 const closeModal = () => modal.classList.add('is-hidden');
 
 modalBtnOpen.addEventListener('click', openModal);
+modalBtnMobileOpen.addEventListener('click', openModal);
 modalBtnClose.addEventListener('click', closeModal);
 
 modal.addEventListener('click', (e) => {
